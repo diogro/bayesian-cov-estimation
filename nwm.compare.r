a5a0c9074af7e87a639f4e01d7d2b1f9a1adea6b
diogro <diogro@gmail.com>
2013-03-20 16:08:54 -0300

    Weird comparisons

--- nwm.compare.r
+++ nwm.compare.r
@@ -1,5 +1,25 @@
 load ("~/Dropbox/monkeys.RData")
-scan("~/projects/lemusp-r/matrix.func.r")
+source("~/projects/lemusp-r/matrix.func.r")
 
-raw.cov = scan("./nwm.cov.bayes.csv", character())
-raw.names = scan("./nwm.names.txt", character())
+raw.cov = scan("~/Dropbox/labbio/nwm.cov.bayes.csv", character())
+raw.means = scan("~/Dropbox/labbio/nwm.means.bayes.csv", character())
+cov.names = scan("~/Dropbox/labbio/nwm.names.txt", character())
+
+cov.bayes = list()
+means.bayes = list()
+for (i in 1:length(cov.names)){
+    cov.bayes[[i]] = matrix(as.numeric(unlist(strsplit(raw.cov[[i]],';'))), ncol=39)
+    means.bayes[[i]] = as.vector(as.numeric(unlist(strsplit(raw.means[[i]],';'))))
+}
+names(cov.bayes) = cov.names
+names(means.bayes) = cov.names
+
+for (i in cov.names){
+    print (i)
+    print( RandomSkewers((main.data[i][[1]])$ed.cov, cov.bayes[i][[1]]))
+}
+
+for (i in cov.names){
+    print (i)
+    print( Norm(colMeans((main.data[i][[1]])$ed)-means.bayes[i][[1]]))
+}
