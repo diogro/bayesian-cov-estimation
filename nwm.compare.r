d5673ff562d6d801c8315530901f65236f4384fc
diogro <diogro@gmail.com>
2013-04-03 14:31:39 -0300

    nwm compare

--- nwm.compare.r
+++ nwm.compare.r
@@ -1,8 +1,8 @@
-load ("~/Dropbox/monkeys.RData")
+load ("~/Dropbox/labbio/cov_bayes_data/monkeys.RData")
 source("~/projects/lemusp-r/matrix.func.r")
 
-raw.cov = scan("~/Dropbox/labbio/cov_bayes_data/nwm.cov.bayes.csv", character())
-raw.means = scan("~/Dropbox/labbio/cov_bayes_data/nwm.means.bayes.csv", character())
+raw.cov = scan("~/Dropbox/labbio/cov_bayes_data/nwm.b.matrix.cov.bayes.csv", character())
+raw.means = scan("~/Dropbox/labbio/cov_bayes_data/nwm.b.matrix.means.bayes.csv", character())
 cov.names = scan("~/Dropbox/labbio/cov_bayes_data/nwm.names.txt", character())
 
 cov.bayes = list()
@@ -20,7 +20,6 @@ j = 0
 for (i in cov.names){
     print (i)
     rs.results[j] = RandomSkewers((main.data[i][[1]])$ed.cov, cov.bayes[i][[1]])[[1]]
-    mean.results[j] = Norm(solve((cov.bayes[i][[1]]),
-                                 colMeans((main.data[i][[1]])$ed) - solve((cov.bayes[i][[1]]), means.bayes[i][[1]])))
+    mean.results[j] = Norm((colMeans((main.data[i][[1]])$ed) - means.bayes[i][[1]]))/39
     j = j + 1
 }
