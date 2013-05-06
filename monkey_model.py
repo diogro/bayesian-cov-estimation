d57e7111580364b44cbaf4b8b49d8f62922470e8
Edgar Z. Alvarenga <edgar@ymonad.com>
2013-05-06 16:30:26 -0300

    Using generic effects tree creator

--- monkey_model.py
+++ monkey_model.py
@@ -3,10 +3,13 @@ import numpy as np
 import pymc as pm
 import noise_control as nc
 import dendropy
+import operator
+from collections import OrderedDict
 
 t = dendropy.Tree.get_from_path("./small.nwm.tree.nw", "newick")
 num_leafs = len(t.leaf_nodes())
 num_traits = 39
+effects = ['SUB', 'SEX']
 
 data = pd.read_csv("./monkey.data.csv")
 raw_matrices = pd.read_csv("./monkey.matrices.csv")
@@ -29,10 +32,10 @@ for i in range(1, len(monkey_labels)):
 # Tirando quem nao esta na filogenia e trocando os keys
 
 for i in range(len(monkey_labels)):
-    if(t.find_node_with_taxon_label(monkey_labels[i]) is not None):
+    if t.find_node_with_taxon_label(monkey_labels[i]):
         new_key = str(t.find_node_with_taxon_label(monkey_labels[i]))
         node_sample_size[new_key] = node_sample_size.pop(monkey_labels[i])
-        if( node_sample_size[new_key] < 60):
+        if node_sample_size[new_key] < 60:
             node_matrices[new_key] = make_symetric(nc.noise_control(node_matrices.pop(monkey_labels[i])))
         else:
             node_matrices[new_key] = node_matrices.pop(monkey_labels[i])
@@ -57,7 +60,7 @@ def matrix_mean(child_labels):
 # Calculando as matrizes e tamanhos amostrais para todos os nodes
 
 for n in t.postorder_node_iter():
-    if ((str(n) in node_matrices) is False):
+    if str(n) not in node_matrices:
         node_matrices[str(n)], node_sample_size[str(n)] = matrix_mean(n.child_nodes())
 
 # Agora comeca o PyMC
@@ -94,33 +97,42 @@ for n in t.nodes()[1:]:
     tree_idx[str(n)] = len(theta) - 1
     i = i + 1
 
-
-def fixed_effect(effect):
+def mk_fixed_effects(effects):
     factor_effects = {}
     for n in t.leaf_nodes():
-        factor_list = list(data.ix[data['species'] == str(n.taxon), effect])
-        factor_effects[str(n)] = [factor_list[1]]
-        for factor in factor_list[1:]:
-            if not (any(factor in s for s in factor_effects[str(n)])):
-                factor_effects[str(n)].append(factor)
+       print effects[0], effects[1:], str(n)
+       factor_effects[str(n)] = mk(n, effects[0], effects[1:])
     return factor_effects
 
-sub_effects = fixed_effect('SUB')
-sex_effects = fixed_effect('SEX')
+def mk(s, e0, es):
+    filtered = set(data.ix[data['species'] == str(s.taxon), e0])
+    print filtered
+    if len(es) < 1:
+        return list(filtered)
+    else:
+        return {k: mk(s, es[0], es[1:] if len(es) > 1 else []) for k in filtered}
 
 data_list = []
-for n in t.leaf_nodes():
-    leaf_idx = tree_idx[str(n)]
-    for sub in sub_effects[str(n)]:
-        for sex in sex_effects[str(n)]:
-            theta.append(pm.MvNormalCov('theta_{}_{}_{}'.format(n.taxon, str(sub), sex),
-                                        mu=theta[leaf_idx],
-                                        C=np.eye(num_traits),
-                                        value=np.zeros(num_traits)))
-            data_list.append(pm.MvNormalCov('data_{}_{}_{}'.format(n.taxon, str(sub), sex),
-                                            mu=theta[len(theta)-1],
-                                            C=sigma[leaf_idx],
-                                            value=np.array(data.ix[(data['species'] == str(n.taxon)) &
-                                                                   (data['SUB'] == sub) &
-                                                                   (data['SEX'] == sex), 0:num_traits]),
-                                            observed=True))
+
+effects_tree = mk_fixed_effects(effects)
+
+#for n in t.leaf_nodes():
+#    leaf_idx = tree_idx[str(n)]
+#
+##    for e in effects.keys():
+##        if len(effects[e]) > 1:
+#
+#
+#    for sub in sub_effects[str(n)]:
+#        for sex in sex_effects[str(n)]:
+#            theta.append(pm.MvNormalCov('theta_{}_{}_{}'.format(n.taxon, str(sub), sex),
+#                                        mu=theta[leaf_idx],
+#                                        C=np.eye(num_traits),
+#                                        value=np.zeros(num_traits)))
+#            data_list.append(pm.MvNormalCov('data_{}_{}_{}'.format(n.taxon, str(sub), sex),
+#                                            mu=theta[len(theta)-1],
+#                                            C=sigma[leaf_idx],
+#                                            value=np.array(data.ix[(data['species'] == str(n.taxon)) &
+#                                                                   (data['SUB'] == sub) &
+#                                                                   (data['SEX'] == sex), 0:num_traits]),
+#                                            observed=True))
