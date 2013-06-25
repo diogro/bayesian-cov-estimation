59c01b1976050e91e598b7dfd8838591c24d360c
diogro <diogro@gmail.com>
2013-06-25 15:33:48 -0300

    more model fixes for paths

--- models/model_test.py
+++ models/model_test.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import numpy as np
 import pymc as pm
-import noise_control as nc
 import dendropy
 import operator
+import os, sys, inspect
+
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../utils")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+import noise_control as nc
+
 
 t = dendropy.Tree.get_from_string("(B, ((C, E),(A,D)))", "newick")
 
